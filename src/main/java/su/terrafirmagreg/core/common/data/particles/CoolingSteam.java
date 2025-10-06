a59760920dc9359b6728631c362c678037240a74
GameStar <56610486+BlueBoat29@users.noreply.github.com>
2025-10-06 16:42:28 -0500

    Update CoolingSteam.java

--- src/main/java/su/terrafirmagreg/core/common/data/particles/CoolingSteam.java
+++ src/main/java/su/terrafirmagreg/core/common/data/particles/CoolingSteam.java
@@ -6,10 +6,12 @@ import net.minecraft.client.particle.*;
 import net.minecraft.world.phys.Vec2;
 
 public class CoolingSteam extends TextureSheetParticle {
-    private final float xWind;
-    private final float zWind;
-    private final float verticalSpeed = 0.6f;
-    private final float speed = 0.4f;
+    private float xWind;
+    private float zWind;
+    private final float xMod;
+    private final float zMod;
+    private final float verticalSpeed = 0.45f;
+    private final float speed = 0.6f;
 
     public ParticleRenderType getRenderType() {
         return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
@@ -18,21 +20,38 @@ public class CoolingSteam extends TextureSheetParticle {
     public CoolingSteam(ClientLevel level, double x, double y, double z) {
         super(level, x, y, z);
         Vec2 wind = ClimateRenderCache.INSTANCE.getWind();
-        this.lifetime = 100;
+        this.lifetime = 180;
         this.gravity = 0;
-        this.age = this.random.nextInt(20);
+        this.age = this.random.nextInt(40);
+        this.hasPhysics = false;
 
-        xWind = wind.x;
-        zWind = wind.y;
+        xMod = 1 + this.random.nextFloat();
+        zMod = 1 + this.random.nextFloat();
 
-        this.scale(this.random.nextFloat() * 8F + 2F);
-        System.out.println("Cooling Steam Constructor");
+        xWind = wind.x * xMod;
+        zWind = wind.y * zMod;
+        this.setAlpha(.5f);
+
+        this.scale(this.random.nextFloat() * 8F + 3F);
     }
+
+    private int ticker = 0;
 
     public void tick() {
         super.tick();
+        if (ticker == 100) {
+            Vec2 wind = ClimateRenderCache.INSTANCE.getWind();
+            xWind = wind.x * xMod;
+            zWind = wind.y * zMod;
 
-        this.yd = (age * -0.01f + 1) * verticalSpeed;
+            // this.setAlpha(1 - (float) age / lifetime);
+
+            ticker = 0;
+        } else {
+            ticker++;
+        }
+
+        this.yd = (age * -0.005f + 1.5) * verticalSpeed;
 
         this.xd = xWind * speed;
         this.zd = zWind * speed;
